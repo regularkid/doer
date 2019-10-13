ed6aadbf4fcb49ea892ea07973f18880fdbe0048
Bryan Perfetto <regularkid@gmail.com>
2019-10-12 21:14:11 -0700

    Code cleanup and fixed line buffer overflow. Added ESC=clear current line input.

--- Source/Console.cpp
+++ Source/Console.cpp
@@ -1,85 +1,109 @@
-#include <Windows.h>
 #include <iostream>
 #include "Console.h"
 
 Console::Console()
 {
 	m_commands.insert({ "test", Command("test") });
+
+	m_stdIn = GetStdHandle(STD_INPUT_HANDLE);
+	m_stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	// No auto echo to std out
+	SetConsoleMode(m_stdIn, 0);
 }
 
 void Console::Run()
 {
-	HANDLE hStdout, hStdin;
-
-	hStdin = GetStdHandle(STD_INPUT_HANDLE);
-	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	SetConsoleMode(hStdin, 0);
-
-	std::string prompt = "> ";
-	std::string curInput;
 	while (1)
+	{		
+		ProcessInput();
+		UpdateOutput();
+	}
+}
+
+void Console::ProcessInput()
+{
+	DWORD numInputsRead = 0;
+	INPUT_RECORD inputRead;
+	ReadConsoleInput(m_stdIn, &inputRead, 1, &numInputsRead);
+
+	// Key was pressed
+	if (inputRead.Event.KeyEvent.bKeyDown)
 	{
-		INPUT_RECORD irInput;
-		DWORD InputsRead = 0;
-
-		ReadConsoleInput(hStdin, &irInput, 1, &InputsRead);
-
-		if (irInput.Event.KeyEvent.bKeyDown)
+		WORD virtualKey = inputRead.Event.KeyEvent.wVirtualKeyCode;
+		switch (virtualKey)
 		{
-			WORD virtualKey = irInput.Event.KeyEvent.wVirtualKeyCode;
-			if (virtualKey == VK_RETURN)
+			// Enter
+			case VK_RETURN:
 			{
-				// Process command here
 				SetCursorPos(0, GetCursorPosY() + 1);
-				curInput.clear();
-			}
-			else if (virtualKey == VK_BACK)
+				m_curInput.clear();
+			} break;
+
+			// Backspace
+			case VK_BACK:
 			{
-				if (!curInput.empty())
+				if (!m_curInput.empty())
 				{
-					curInput.pop_back();
+					m_curInput.pop_back();
 				}
-			}
-			else
+			} break;
+
+			// Escape
+			case VK_ESCAPE:
 			{
-				CHAR asciiKey = irInput.Event.KeyEvent.uChar.AsciiChar;
-				if (asciiKey != 0)
+				SetCursorPos(0, GetCursorPosY());
+				m_curInput.clear();
+			} break;
+
+			// Normal character
+			default:
+			{
+				CHAR asciiKey = inputRead.Event.KeyEvent.uChar.AsciiChar;
+				if (asciiKey != 0 && !CurLineHasMaxCharacters())
 				{
-					curInput += asciiKey;
+					m_curInput += asciiKey;
 				}
 			}
 		}
-
-		CONSOLE_SCREEN_BUFFER_INFO screenInfo;
-		GetConsoleScreenBufferInfo(hStdout, &screenInfo);
-
-		DWORD numCharsWritten;
-		COORD linePos;
-		linePos.X = 0;
-		linePos.Y = screenInfo.dwCursorPosition.Y;
-		std::string curLine = prompt + curInput;
-		curLine += std::string(screenInfo.dwMaximumWindowSize.X - curLine.size() - 1, ' ');
-		WriteConsoleOutputCharacter(hStdout, curLine.c_str(), curLine.size(), linePos, &numCharsWritten);
-
-		SetCursorPos(static_cast<SHORT>(prompt.size() + curInput.size()), GetCursorPosY());
 	}
+}
+
+void Console::UpdateOutput()
+{
+	// Fill entire line: prompt + current input + fill spaces to end
+	std::string curLine = m_prompt + m_curInput;
+	curLine += std::string(GetCursorMaxX() - curLine.size() - 1, ' ');
+
+	DWORD numCharsWritten = 0;
+	COORD linePos;
+	linePos.X = 0;
+	linePos.Y = GetCursorPosY();
+	WriteConsoleOutputCharacter(m_stdOut, curLine.c_str(), curLine.size(), linePos, &numCharsWritten);
+
+	// Cursor should be 1 char past prompt + current input
+	SetCursorPos(static_cast<SHORT>(m_prompt.size() + m_curInput.size()), GetCursorPosY());
 }
 
 int Console::GetCursorPosX() const
 {
 	CONSOLE_SCREEN_BUFFER_INFO screenInfo;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screenInfo);
-
+	GetConsoleScreenBufferInfo(m_stdOut, &screenInfo);
 	return static_cast<int>(screenInfo.dwCursorPosition.X);
 }
 
 int Console::GetCursorPosY() const
 {
 	CONSOLE_SCREEN_BUFFER_INFO screenInfo;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &screenInfo);
-
+	GetConsoleScreenBufferInfo(m_stdOut, &screenInfo);
 	return static_cast<int>(screenInfo.dwCursorPosition.Y);
+}
+
+int Console::GetCursorMaxX() const
+{
+	CONSOLE_SCREEN_BUFFER_INFO screenInfo;
+	GetConsoleScreenBufferInfo(m_stdOut, &screenInfo);
+	return static_cast<int>(screenInfo.dwMaximumWindowSize.X);
 }
 
 void Console::SetCursorPos(int x, int y)
@@ -87,5 +111,12 @@ void Console::SetCursorPos(int x, int y)
 	COORD cursorPos;
 	cursorPos.X = static_cast<SHORT>(x);
 	cursorPos.Y = static_cast<SHORT>(y);
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPos);
+	SetConsoleCursorPosition(m_stdOut, cursorPos);
+}
+
+bool Console::CurLineHasMaxCharacters() const
+{
+	const int maxCharacters = GetCursorMaxX() - 1;
+	const int numCharacters = static_cast<int>(m_prompt.size()) + static_cast<int>(m_curInput.size());
+	return numCharacters >= maxCharacters;
 }
--- Source/Console.h
+++ Source/Console.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <Windows.h>
 #include <unordered_map>
 #include "Command.h"
 
@@ -11,9 +12,19 @@ public:
 	void Run();
 
 private:
+	void ProcessInput();
+	void UpdateOutput();
+
 	int GetCursorPosX() const;
 	int GetCursorPosY() const;
+	int GetCursorMaxX() const;
 	void SetCursorPos(int x, int y);
+	bool CurLineHasMaxCharacters() const;
 
+	std::string m_prompt = "> ";
+	std::string m_curInput;
 	std::unordered_map<std::string, Command> m_commands;
+
+	HANDLE m_stdIn = NULL;
+	HANDLE m_stdOut = NULL;
 };
